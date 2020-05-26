27e41c14908b8e59abd31ebe281c7fe7bcf04596
Ramakrishnan CS <ramakrishnan.cs@phablecare.com>
2020-05-26 12:29:50 +0530

    Fixed UI breaking issues in RN 0.62+ upgrades

--- src/components/field/index.js
+++ src/components/field/index.js
@@ -137,13 +137,16 @@ export default class TextField extends PureComponent {
     y1: 0,
   };
 
-  static getDerivedStateFromProps({ error }, state) {
+  static getDerivedStateFromProps({ error, value }, state) {
+    let update = {};
     /* Keep last received error in state */
     if (error && error !== state.error) {
-      return { error };
+      update.error = error;
     }
-
-    return null;
+    if (value !== state.text) {
+      update.text = value;
+    }
+    return update;
   }
 
   constructor(props) {
